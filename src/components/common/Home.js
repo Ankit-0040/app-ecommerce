a4581b5d81ec2a6f0f44651f1fe8f37a3ddbbc68
Ankit-0040 <ankitssingh1912@gmail.com>
2024-03-07 17:45:06 +0530

    css added

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,7 +1,9 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../Products/ProductCard";
 import TextField from '@mui/material/TextField';
 import MySlider from "../Products/Slider";
+import { Grid } from '@mui/material';
+import '../css/Home.css'
 
 function Home() {
     const [data, setData] = useState({});
@@ -12,38 +14,38 @@ function Home() {
             .then((res) => setData(res))
             .catch((error) => console.error("Error fetching data:", error));
     }, []);
-    
+
     const filteredProducts = data.products && data.products.filter(product => {
         return product.title.toLowerCase().includes(searchTerm.toLowerCase());
     });
 
     return (
         <div>
-              <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+            <div className="search">
                 <TextField
                     id="standard-search"
                     label="Search Product"
                     type="search"
                     variant="standard"
-                    sx={{ width: '400px' }} 
+                    style={{ minWidth: '200px', maxWidth: '400px', width: '100%' }}
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
+
             </div>
 
             <div >
-              
-                <MySlider products={filteredProducts} />
-                
-            </div>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', justifyContent: 'center' }}>
-                {filteredProducts && filteredProducts.map(product => (
-                    
-                    <ProductCard key={product.id} product={product} style={{ margin: '10px' }} />
-                   
-                ))}
+                <MySlider products={filteredProducts} />
+
             </div>
+            <Grid container spacing={2}>
+                {filteredProducts && filteredProducts.map(product => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <ProductCard product={product} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     );
 }
