f585266a43b06330178aadb8be78262ac7b9f9cc
Ankit-0040 <ankitssingh1912@gmail.com>
2024-03-07 10:36:03 +0530

    breadcrumbs added

--- src/components/common/Cart.js
+++ src/components/common/Cart.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import CartCard from '../Products/CartCard'
 import ClearModal from '../Products/ClearModal';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import { Link } from 'react-router-dom'; 
 
 function Cart() {
     const { cartItems } = useSelector((state) => state.cart)
@@ -9,6 +12,14 @@ function Cart() {
     const total = cartItems.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
     return (
         <div>
+    <div role="presentation">
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" to="/">
+            Home
+        </Link>
+        <Typography color="text.primary">Cart</Typography>
+      </Breadcrumbs>
+    </div>
             <div style={{ textAlign: 'center' }}>
                 <h4>Cart Items</h4>
             </div>
