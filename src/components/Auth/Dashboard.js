9d9035a367a5bad6c65ef5b3a1339a8ed69bda47
Ankit-0040 <ankitssingh1912@gmail.com>
2024-03-07 16:42:12 +0530

    handleBrowserRouting

--- src/components/Auth/Dashboard.js
+++ src/components/Auth/Dashboard.js
@@ -1,44 +1,67 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 import About from '../common/About';
 import NavBar from '../common/NavBar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "../common/Home";
-import WishList from "../common/WhishList";
-import Cart from "../common/Cart";
-import LoginPage from "./LoginPage";
-import  AuthProvider  from "../../context/AuthProvider";
+import Home from '../common/Home';
+import WishList from '../common/WhishList';
+import Cart from '../common/Cart';
+import LoginPage from './LoginPage';
+import AuthProvider from '../../context/AuthProvider';
 import ProductPage from '../common/ProductPage';
 
 function Dashboard() {
   return (
     <Router>
       <AuthProvider>
-        <div>
-          <NavBar title="Shopping Cart" aboutText="About" />
-          <div className="container my-3">
-            <Switch>
-              <Route exact path="/about">
-                <About />
-              </Route>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/cart">
-                <Cart />
-              </Route>
-              <Route exact path="/wishlist">
-                <WishList />
-              </Route>
-              <Route exact path="/login">
-                <LoginPage />
-              </Route>
-              <Route exact path="/productdetails/:id">
-                <ProductPage />
-              </Route>
-            </Switch>
-          </div>
-        </div>
+        <MainContent />
       </AuthProvider>
     </Router>
+  );
+}
+
+function MainContent() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const handlePopstate = () => {
+      // const currentPath = window.location.pathname;
+      // console.log(currentPath);
+      history.push('/');
+    };
+
+    window.addEventListener('popstate', handlePopstate);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopstate);
+    };
+  }, [history]);
+
+  return (
+    <div>
+      <NavBar title="Shopping Cart" aboutText="About" />
+      <div className="container my-3">
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/wishlist">
+            <WishList />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/productdetails/:id">
+            <ProductPage />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   );
 }
 
